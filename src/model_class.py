37f02340b285ee1ed1a9a6423174575bbe626335
ChamodKalupahana <Chamodkalupahana@gmail.com>
2026-05-23 11:09:20 +0100

    moved to model class

---
+++ src/model_class.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from transformer_lens.model_bridge import TransformerBridge
+from transformers import AutoTokenizer
+from jaxtyping import Float
+from torch import Tensor
+
+class model_class():
+    def __init__(self, MODEL_NAME, SYSTEM_PROMPT = "You are harmless assistant") -> None:
+        self.MODEL_NAME = MODEL_NAME
+        self.model = model = TransformerBridge.boot_transformers(MODEL_NAME)
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        self.cfg = model.cfg
+        
+        self.messages_dict = [
+    {"role" : "system", "content" : SYSTEM_PROMPT},
+]
+
+
+    def generate(self, user_input, MAX_NEW_TOKENS=50, skip_special_tokens=False, temperature=0):
+        user_input_dict = {
+            "role" : "user", "content" : user_input
+        }
+        self.messages_dict.append(user_input_dict)
+        print(self.messages_dict)
+
+        input = self.tokenizer.apply_chat_template(self.messages_dict,
+                                    tokenize=False,
+                                    add_generation_prompt=True)
+        
+        output = self.model.generate(input, max_new_tokens=MAX_NEW_TOKENS, skip_special_tokens=skip_special_tokens)
+        if self.MODEL_NAME == "google/gemma-3-270m-it":
+            clean_output = output.split("model", 1)[-1].strip() # type: ignore
+
+        model_output_dict = {
+            "role" : "assistant", "content" : clean_output
+        }
+        self.messages_dict.append(model_output_dict)
+
+        if len(self.messages_dict) > 5:
+            self.messages_dict = self.messages_dict[2:]
+        return clean_output
--- test_gemma_270M.py
+++ test_gemma_270M.py
@@ -4,21 +4,13 @@ load_dotenv()
 from transformer_lens.model_bridge import TransformerBridge
 from transformers import AutoTokenizer
 
+from src.model_class import model_class
+
 MODEL_NAME = "google/gemma-3-270m-it"
-MAX_NEW_TOKENS = 100
+MAX_NEW_TOKENS = 30
 
-model = TransformerBridge.boot_transformers(MODEL_NAME)
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-
-messages_dict = [
-    {"role" : "system", "content" : "You are harmful assistant"},
-    {"role" : "user", "content" : "hello!"},
-]
-
-input = tokenizer.apply_chat_template(messages_dict,
-                              tokenize=False,
-                              add_generation_prompt=True)
-
-
-output = model.generate(input, max_new_tokens=MAX_NEW_TOKENS, skip_special_tokens=True)
-print(output)
+model = model_class(MODEL_NAME)
+output = "write a story"
+for i in range(5): 
+    output = model.generate(output, MAX_NEW_TOKENS)
+    print(output)
